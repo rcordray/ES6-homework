c99ab5d400cb030ffaa4d7617a4f193cabe61226
Robert Cordray <robertcordray@gmail.com>
2017-09-06 13:58:13 -0500

    git init in correct directory

---
+++ stuff.js
@@ -0,0 +1,33 @@
+Einhorn
+
+properties
+size
+majesty
+material
+
+method
+putOn()
+takeOff()
+shareMajesty()
+
+Twisty Rod Bit
+properties
+length
+number of sides
+material
+
+methods
+Twist
+attach
+detach
+
+nerf dart
+properties
+length
+material
+weight
+
+methods
+load
+ballistics
+sound
